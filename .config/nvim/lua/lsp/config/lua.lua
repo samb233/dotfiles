a178fbc357761f917f353d4e7e401d244f9e5912
jiesamb <samb233@hotmail.com>
2022-10-28 10:07:18 +0800

    fix: fix lsp settings

--- .config/nvim/lua/lsp/config/lua.lua
+++ .config/nvim/lua/lsp/config/lua.lua
@@ -41,13 +41,13 @@ local opts = {
 		-- 绑定快捷键
 		require("keybindings").mapLSP(buf_set_keymap)
 		-- 保存时自动格式化
-		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
 	end,
 }
 
 -- 查看目录等信息
 return {
 	on_setup = function(server)
-		server:setup(opts)
+		server.setup(opts)
 	end,
 }
--- .config/nvim/lua/lsp/config/python.lua
+++ .config/nvim/lua/lsp/config/python.lua
@@ -1,6 +1,6 @@
 return {
 	on_setup = function(server)
-		server:setup({
+		server.setup({
 			-- capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 			on_attach = function(client, bufnr)
 				client.server_capabilities.documentFormattingProvider = false
