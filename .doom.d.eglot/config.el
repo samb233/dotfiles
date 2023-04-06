ae22761d9a9293eb721b8f8af4c15f53b8d23866
jiesamb <samb233@hotmail.com>
2023-04-06 21:14:32 +0800

    emacs: add eglot config in .doom.d.eglot

---
+++ .doom.d.eglot/config.el
@@ -0,0 +1,514 @@
+(server-start)
+
+(setq user-full-name "Jie Samb"
+      user-mail-address "samb233@hotmail.com")
+
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
+
+(pushnew! default-frame-alist '(width . 80) '(height . 50))
+;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; (add-to-list 'default-frame-alist '(alpha-background . 95))
+;; (add-to-list 'default-frame-alist (cons 'alpha 90))
+
+(setq frame-title-format
+      '((:eval (if (buffer-file-name)
+                   (abbreviate-file-name (buffer-file-name))
+                 "%b"))))
+
+(setq doom-font (font-spec :family "IBM Plex Mono Medm" :size 11.0))
+(setq doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font"))
+(setq doom-unicode-font (font-spec :family "Sarasa Mono SC" ))
+
+(setq scroll-margin 9)
+(setq mouse-wheel-scroll-amount '
+      (3
+       ((shift) . hscroll)
+       ((meta))
+       ((control) . text-scale)))
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
+;; (pixel-scroll-precision-mode t)
+
+(setq doom-theme 'doom-tomorrow-day)
+
+(setq evil-emacs-state-cursor 'bar)
+
+(setq display-line-numbers-type 'relative)
+
+(setq doom-modeline-modal t)
+(setq doom-modeline-modal-icon nil)
+(setq doom-modeline-buffer-encoding t)
+(setq doom-modeline-vcs-max-length 20)
+(setq doom-modeline-height 28)
+(setq doom-modeline-buffer-modification-icon nil)
+(setq doom-modeline-buffer-state-icon nil)
+(after! doom-modeline
+  (set-face-attribute 'mode-line-active nil :background "#f4f4f4")
+  (setq doom-modeline-buffer-file-name-style 'buffer-name)
+  )
+
+(after! persp-mode
+  (setq-hook! 'persp-mode-hook uniquify-buffer-name-style 'forward)
+  )
+
+(defalias 'evil-insert-state 'evil-emacs-state)
+(define-key evil-emacs-state-map (kbd "<escape>") 'evil-normal-state)
+;; (setq evil-disable-insert-state-bindings t)
+
+(evil-define-key 'visual 'global
+  (kbd "J") 'drag-stuff-down
+  (kbd "K") 'drag-stuff-up)
+
+(evil-define-key 'normal 'global (kbd "C-s") 'consult-line)
+(map! "C-s" #'consult-line)
+(setq consult-line-start-from-top t)
+
+(map! "C-v" #'yank)
+(map! "M-v" #'yank)
+
+(evil-ex-define-cmd "q" 'kill-this-buffer)
+(evil-ex-define-cmd "quit" 'evil-quit)
+
+(evil-define-key 'normal 'global
+  (kbd "q") nil)
+
+(map! :leader
+      :desc "ace-select-window" "w a"   #'ace-select-window
+      :desc "ace-select-window" "w w"   #'ace-select-window
+      )
+
+(setq undo-no-redo t)
+(setq evil-want-fine-undo t)
+
+(after! recentf
+  (setq recentf-max-saved-items 1000)
+  )
+
+(add-to-list 'auto-mode-alist '("\\.vpy\\'" . python-mode))
+
+(setq auto-revert-check-vc-info t)
+
+(after! evil
+  (setq evil-emacs-state-tag "INSERT")
+  (setq evil-insert-state-tag "INSERT")
+  (setq evil-motion-state-tag "MOTION")
+  (setq evil-normal-state-tag "NORMAL")
+  (setq evil-replace-state-tag "REPLACE")
+  (setq evil-operator-state-tag "OPERATOR")
+  (setq evil-visual-char-tag "VISUAL")
+  (setq evil-visual-line-tag "V-LINE")
+  (setq evil-visual-block-tag "V-BLOCK")
+  (setq evil-visual-screen-line-tag "V-SCREEN")
+  )
+
+(setq magit-clone-default-directory "~/Codes/Lab/")
+
+(after! persp-mode
+  (map! :leader
+        "," #'consult-buffer
+        "<" #'+vertico/switch-workspace-buffer )
+  )
+
+;; (after! lsp-mode
+;;   (setq lsp-lens-enable nil)
+;;   (setq lsp-enable-snippet nil)
+;;   (setq lsp-signature-doc-lines 5)
+;;   (setq lsp-ui-sideline-diagnostic-max-lines 2)
+;;   )
+
+;; (map! :leader
+;;       (:prefix-map ("l" . "LSP")
+;;        :desc "LSP rename" "n" #'lsp-rename
+;;        :desc "LSP find definitions" "f" #'lsp-find-definition
+;;        :desc "LSP find reference" "r" #'lsp-find-references
+;;        (:prefix ("w" . "workspace")
+;;         :desc "LSP change workspace" "w" #'lsp-workspace-folders-switch
+;;         :desc "LSP restart workspace" "r" #'lsp-workspace-restart
+;;         )
+;;        ))
+
+(map! :leader
+      (:prefix-map ("l" . "LSP")
+       :desc "LSP rename" "n" #'eglot-rename
+       :desc "LSP find definitions" "f" #'xref-find-definitions
+       :desc "LSP find reference" "r" #'xref-find-references
+       (:prefix ("w" . "workspace")
+        ;; :desc "LSP change workspace" "w" #'
+        :desc "LSP restart workspace" "r" #'eglot-reconnect
+        )
+       ))
+
+(after! eglot
+  (set-face-attribute 'eglot-highlight-symbol-face nil :background "#d6d4d4")
+  (setq eglot-events-buffer-size 0)
+  (setq eldoc-echo-area-use-multiline-p nil)
+  (setq eglot-stay-out-of '(flymake snippet))
+  (set-popup-rule! "^\\*eldoc*" :size 0.3 :modeline t :quit t)
+  (set-lookup-handlers! 'eglot--managed-mode
+    :definition      #'xref-find-definitions
+    :references      #'xref-find-references
+    :implementations #'eglot-find-implementation
+    :type-definition #'eglot-find-typeDefinition
+    :documentation   #'eldoc)
+  )
+
+(after! corfu
+  (setq corfu-preselect 'prompt)
+  ;; (setq corfu-preview-current nil)
+  (setq corfu-auto-prefix 1)
+  (setq corfu-auto-delay 0.1)
+  (map! :map corfu-map
+        "TAB" #'corfu-next
+        [tab] #'corfu-next
+        "S-TAB" #'corfu-previous
+        [backtab] #'corfu-previous
+        "C-j" #'corfu-next
+        "C-k" #'corfu-previous
+        "C-l" #'corfu-insert-separator
+        "C-i" #'corfu-info-documentation
+        )
+  (map! :map global-map
+        "C-S-p" #'+corfu-files)
+  )
+
+(add-hook! 'evil-emacs-state-exit-hook #'corfu-quit)
+
+(use-package! kind-all-the-icons)
+
+(after! corfu
+  (add-to-list 'corfu-margin-formatters #'kind-all-the-icons-margin-formatter)
+  )
+
+(use-package! dirvish
+  :init
+  (dirvish-override-dired-mode)
+  :custom
+  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+   '(("h" "~/"                          "Home")
+     ("c" "~/Codes/"                    "Codes")
+     ("t" "~/Codes/Try/"                "Try")
+     ("p" "~/Codes/Projects/"           "Projects")
+     ("r" "~/Codes/Reading/"            "Reading")
+     ("D" "~/Documents/"                "Documents")
+     ("w" "~/Works/"                    "Works")
+     ("d" "~/Downloads/"                "Downloads")
+     ("P" "~/Pictures/"                 "Pictures")
+     ("v" "~/Videos/"                   "Videos")
+     ("s" "~/Shared/"                   "Shared")
+     ("n" "~/Notes/"                    "Notes")
+     ("b" "~/Books/"                    "Books")
+     ("M" "/mnt/"                       "Drives")
+     ("T" "~/.local/share/Trash/files/" "TrashCan")
+     ))
+  :config
+  ;; (dirvish-peek-mode) ; Preview files in minibuffer
+  (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
+  ;; (setq dirvish-reuse-session nil) ; disable session reuse
+  ;; (setq dirvish--debouncing-delay 2)
+  (setq dirvish-async-listing-threshold 10000)
+  (setq dirvish-use-mode-line nil)
+  ;; (setq dirvish-default-layout '(0 0.4 0.6))
+  ;; (setq dirvish-mode-line-format
+  ;;       '(:left (sort symlink) :right (omit yank index)))
+  (setq dirvish-header-line-height '41)
+  ;; (setq dirvish-mode-line-height '46)
+  (setq dirvish-attributes
+        '(file-time all-the-icons file-size collapse subtree-state vc-state git-msg))
+  (setq delete-by-moving-to-trash t)
+  (setq dired-listing-switches
+        "-l --almost-all --human-readable --group-directories-first --no-group --time-style=iso")
+  (setq dirvish-fd-default-dir "/home/jiesamb/")
+  (setq dirvish-open-with-programs
+        `(
+          (,dirvish-audio-exts . ("mpv" "%f"))
+          (,dirvish-video-exts . ("mpv" "%f"))
+          (,dirvish-image-exts . ("eog" "%f"))
+          (("doc" "docx") . ("wps" "%f"))
+          (("ppt" "pptx") . ("wpp" "%f"))
+          (("xls" "xlsx") . ("et" "%f"))
+          (("pdf") . ("evince" "%f"))
+          (("odt" "ods" "rtf" "odp") . ("libreoffice" "%f"))
+          (("epub") . ("koodo-reader" "%f"))
+          ))
+  (setq dirvish-header-line-format '(:left (path) :right (yank sort index)))
+  (setq dirvish-path-separators (list "  ~" "  " "/"))
+  (setq dirvish-side-display-alist `((side . right) (slot . -1)))
+  (setq dirvish-side-width 40)
+  (setq dirvish-side-auto-close t)
+  (map! :map dirvish-mode-map
+        :n "h" #'dired-up-directory
+        :n "l" #'dired-find-file
+        :n "e" #'dired-create-empty-file
+        :n "." #'dired-omit-mode
+        :n "q" #'dirvish-quit
+        :n "s" #'dirvish-quicksort
+        :n "a" #'dirvish-quick-access
+        :n "F" #'dirvish-fd
+        :n "S" #'dirvish-fd-switches-menu
+        :n "y" #'dirvish-yank-menu
+        :n "f" #'dirvish-file-info-menu
+        :n "H" #'dirvish-history-jump
+        :n "TAB" #'dirvish-subtree-toggle
+        :n [backtab] #'dirvish-subtree-up
+        "M-t" #'dirvish-layout-toggle
+        "M-j" #'dirvish-fd-jump
+        "M-m" #'dirvish-mark-menu )
+  )
+
+(map! :leader
+      :desc "Open dired" "N" #'dired-jump
+      :desc "Open dirvish" "V" #'dirvish
+      (:prefix ("v" . "my personal bindings")
+       :desc "Open dirvish" "v" #'dirvish
+       :desc "Open Normal Dired" "n" #'dired-jump
+       :desc "Quit dirvish" "q" #'dirvish-quit
+       :desc "Toggle dirvish-side" "s" #'dirvish-side
+       :desc "Fd in dirvish" "F" #'dirvish-fd
+       :desc "Jump using fd" "J" #'dirvish-fd-jump
+       :desc "Jump recent dir" "j" #'consult-dir
+       :desc "Fd find file in dir" "f" #'+vertico/consult-fd
+       :desc "find Item in the buffer" "i" #'consult-imenu
+       :desc "open with other coding system" "c" #'revert-buffer-with-coding-system
+       :desc "change buffer coding system" "C" #'set-buffer-file-coding-system
+       :desc "List processes" "l" #'list-processes
+       :desc "calc mode" "a" #'literate-calc-mode
+       ))
+
+(setq delete-window-choose-selected 'pos)
+
+(after! vterm
+  (setq vterm-max-scrollback 10000)
+  (advice-add #'vterm--redraw :after (lambda (&rest args) (evil-refresh-cursor evil-state)))
+  (set-face-attribute 'vterm-color-black nil :background "#a7a7a7")
+  (remove-hook 'vterm-mode-hook 'hide-mode-line-mode)
+  )
+
+(defun my/eshell-use-git-prompt-theme()
+  (eshell-git-prompt-use-theme 'git-radar)
+  )
+(add-hook! 'eshell-prompt-load-hook #'my/eshell-use-git-prompt-theme)
+
+(map! :leader
+      :desc "Open Vterm" "T" #'+vterm/here
+      :desc "Toggle Eshell" "E" #'+eshell/toggle
+      )
+
+(use-package! docker
+  :config
+  (setq docker-command "podman")
+  (setq docker-compose-command "podman-compose")
+  (setq docker-pop-to-buffer-action '(display-buffer-same-window))
+  (setq docker-run-async-with-buffer-function #'docker-run-async-with-buffer-vterm)
+  (setq docker-container-columns
+        '(
+          (:name "Id" :width 14 :template "{{ json .ID }}" :sort nil :format nil)
+          (:name "Names" :width 12 :template "{{ json .Names }}" :sort nil :format nil)
+          (:name "Status" :width 14 :template "{{ json .Status }}" :sort nil :format nil)
+          (:name "Ports" :width 24 :template "{{ json .Ports }}" :sort nil :format nil)
+          (:name "Image" :width 40 :template "{{ json .Image }}" :sort nil :format nil)
+          (:name "Created" :width 21 :template "{{ json .CreatedAt }}" :sort nil :format
+                 (lambda (x) (format-time-string "%F %T" (date-to-time x))))
+          (:name "Command" :width 20 :template "{{ json .Command }}" :sort nil :format nil)))
+  )
+
+(after! docker-tramp
+  (setq docker-tramp-docker-executable "podman")
+  )
+
+(map! :leader
+      :desc "docker Containers" "o c" #'docker-containers
+      )
+
+(use-package! sis
+  :config
+  (sis-ism-lazyman-config "1" "2" 'fcitx5)
+  ;; enable the /cursor color/ mode
+  ;; (sis-global-cursor-color-mode t)
+  ;; enable the /respect/ mode
+  (sis-global-respect-mode t)
+  ;; enable the /context/ mode for all buffers
+  (sis-global-context-mode t)
+  ;; enable the /inline english/ mode for all buffers
+  ;; (sis-global-inline-mode t)
+  )
+
+(add-hook! 'evil-emacs-state-exit-hook #'sis-set-english)
+(add-hook! 'evil-emacs-state-entry-hook #'sis-context t)
+
+(add-hook! 'evil-emacs-state-exit-hook #'doom-modeline-update-buffer-file-name)
+;; (add-hook! 'evil-emacs-state-exit-hook #'evil-maybe-expand-abbrev)
+
+(setq org-directory "~/Notes")
+(after! org
+  (defun my/org-colors-tomorrow-night ()
+    (interactive)
+    (dolist
+        (face
+         '((org-level-1 1.3 "#81a2be" ultra-bold)
+           (org-level-2 1.2 "#b294bb" extra-bold)
+           (org-level-3 1.1 "#b5bd68" bold)
+           (org-level-4 1.0 "#e6c547" semi-bold)
+           (org-level-5 1.0 "#cc6666" normal)
+           (org-level-6 1.0 "#70c0ba" normal)
+           (org-level-7 1.0 "#b77ee0" normal)
+           (org-level-8 1.0 "#9ec400" normal)))
+      (set-face-attribute (nth 0 face) nil :weight (nth 3 face) :height (nth 1 face) :foreground (nth 2 face)))
+    (set-face-attribute 'org-table nil :weight 'normal :height 1.0 :foreground "#bfafdf"))
+
+  (defun my/org-colors-tomorrow-day()
+    (interactive)
+    (dolist
+        (face
+         '((org-level-1 1.3 "#4271ae" ultra-bold)
+           (org-level-2 1.2 "#8959a8" extra-bold)
+           (org-level-3 1.1 "#b5bd68" bold)
+           (org-level-4 1.0 "#e6c547" semi-bold)
+           (org-level-5 1.0 "#c82829" normal)
+           (org-level-6 1.0 "#70c0ba" normal)
+           (org-level-7 1.0 "#b77ee0" normal)
+           (org-level-8 1.0 "#9ec400" normal)))
+      (set-face-attribute (nth 0 face) nil :weight (nth 3 face) :height (nth 1 face) :foreground (nth 2 face)))
+    (set-face-attribute 'org-table nil :weight 'normal :height 1.0 :foreground "#bfafdf"))
+
+  (my/org-colors-tomorrow-day)
+  (setq org-src-preserve-indentation nil)
+  )
+
+(add-hook! 'org-mode-hook #'toggle-word-wrap)
+
+(setq org-roam-directory "~/Notes/Roam")
+(map! :leader
+      :desc "Zettelkasten by org-roam" "v z" #'org-roam-node-find
+      :desc "org-roam node Insert" "v i" #'org-roam-node-insert
+      )
+
+(setq org-roam-dailies-directory "~/Notes/Daily")
+(setq org-roam-dailies-capture-templates
+      '(("d" "default" entry
+         "* %?"
+         :target (file+head "%<%Y>/%<%Y-%m>/%<%Y-%m-%d>.org"
+                            "#+title: %<%Y-%m-%d %A>\n"))))
+(map! :leader
+      :desc "my Journal today" "J" #'org-roam-dailies-goto-today
+      )
+
+(setq org-roam-capture-templates '(
+          ("d" "Default" plain "%?"
+          :target (file+head "Default/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: \n\n")
+          :unnarrowed t)
+          ("t" "Tech" plain "%?"
+          :target (file+head "Tech/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :tech: \n\n")
+          :unarrowed t)
+          ("b" "Book Notes" plain "%?"
+          :target (file+head "Book/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :book: \n\n")
+          :unnarrowed t)
+          ("w" "Work" plain "%?"
+          :target (file+head "Work/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :work: \n\n")
+          :unnarrowed t)
+          ("a" "Anime" plain "%?"
+          :target (file+head "Anime/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :anime: \n\n")
+          :unnarrowed t)
+          ("v" "Video or VCBs" plain "%?"
+          :target (file+head "Video/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :video: \n\n")
+          :unnarrowed t)
+          ("p" "Project" plain "%?"
+          :target (file+head "Project/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :project: \n\n")
+          :unnarrowed t)
+          ("c" "Coding" plain "%?"
+          :target (file+head "Coding/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :coding: \n\n")
+          :unnarrowed t)
+          ("h" "Thinking" plain "%?"
+          :target (file+head "Thinking/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :thinking: \n\n")
+          :unnarrowed t)
+          ("i" "Inspiration" plain "%?"
+          :target (file+head "Inspiration/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags: :inspiration: \n\n")
+          :unnarrowed t)))
+
+(map! :leader
+      :desc "org-roam capture" "X" #'org-roam-capture
+      :desc "org-roam find node" "Z" #'org-roam-node-find
+      )
+
+(custom-set-faces
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.3))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.2))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.1))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.0))))
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.0))))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.0)))))
+
+(setq deft-extensions '("txt" "tex" "org" "md"))
+(setq deft-directory "~/Notes")
+(setq deft-recursive t)
+
+(defun my/deft-parse-title (file contents)
+  "Parse the given FILE and CONTENTS and determine the title.
+If `deft-use-filename-as-title' is nil, the title is taken to
+be the first non-empty line of the FILE.  Else the base name of the FILE is
+used as title."
+  (let ((begin (string-match "^#\\+[tT][iI][tT][lL][eE]: .*$" contents)))
+    (if begin
+        (string-trim (substring contents begin (match-end 0)) "#\\+[tT][iI][tT][lL][eE]: *" "[\n\t ]+")
+      (deft-base-filename file))))
+
+(advice-add 'deft-parse-title :override #'my/deft-parse-title)
+
+(setq deft-strip-summary-regexp
+      (concat "\\("
+              "[\n\t]" ;; blank
+              "\\|^#\\+[[:alpha:]_]+:.*$" ;; org-mode metadata
+              "\\|^:PROPERTIES:\n\\(.+\n\\)+:END:\n"
+              "\\)"))
+
+(use-package! sh-script
+  :config
+  (set-formatter! 'shfmt
+    '("shfmt" "-ci"
+      ("-i" "%d" (unless indent-tabs-mode tab-width))
+      ("-ln" "%s" (pcase sh-shell (`bash "bash") (`zsh "bash") (`mksh "mksh") (_ "posix")))))
+  )
+
+(defun lsp-go-install-save-hooks ()
+  ;; (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  ;; (add-hook 'before-save-hook #'lsp-organize-imports t t))
+  (defun my-eglot-organize-imports () (interactive)
+         (eglot-code-actions nil nil "source.organizeImports" t))
+  (add-hook 'before-save-hook 'my-eglot-organize-imports nil t)
+  (add-hook 'before-save-hook 'eglot-format-buffer))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+(set-popup-rule! "^\\*format-all-errors*" :size 0.3 :modeline t :quit t)
+
+(setq flycheck-check-syntax-automatically '(save mode-enabled idle-change idle-buffer-switch))
+
+(use-package! fanyi
+  :custom
+  (fanyi-providers '(;; 海词
+                     fanyi-haici-provider
+                     ;; 有道同义词词典
+                     fanyi-youdao-thesaurus-provider
+                     ;; Etymonline
+                     ;; fanyi-etymon-provider
+                     ;; Longman
+                     ;; fanyi-longman-provider
+                     )))
+(set-popup-rule! "^\\*fanyi*" :size 0.3 :modeline t :quit t)
+(map! :leader
+      :desc "Translate word" "v t" #'fanyi-dwim2
+      )
