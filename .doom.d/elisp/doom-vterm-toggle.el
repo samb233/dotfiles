3721537bb6995a83ae9217406ab6150c675c4a99
jiesamb <samb233@hotmail.com>
2023-05-06 18:38:52 +0800

    emacs: 精简doom-vterm-toggle

--- .doom.d/elisp/doom-vterm-toggle.el
+++ .doom.d/elisp/doom-vterm-toggle.el
@@ -7,30 +7,18 @@
 
 (defvar doom-vterm--id nil)
 
-(defun doom-vterm-toggle-directory (arg)
+(defun doom-vterm-toggle-directory ()
   "Toggles a terminal popup window at current directory.
-
 If prefix ARG is non-nil, recreate vterm buffer in the current directory.
-
 Returns the vterm buffer."
-  (interactive "P")
+  (interactive)
   (doom-vterm--configure-project-root-and-display
    1
    (lambda ()
      (let ((buffer-name
-            (format "*doom:vterm-popup:%s*"
-                    (if (bound-and-true-p persp-mode)
-                        (safe-persp-name (get-current-persp))
-                      default-directory)))
+            (format "*doom:vterm-popup:%s*" (file-truename default-directory)))
            confirm-kill-processes
            current-prefix-arg)
-       (when arg
-         (let ((buffer (get-buffer buffer-name))
-               (window (get-buffer-window buffer-name)))
-           (when (buffer-live-p buffer)
-             (kill-buffer buffer))
-           (when (window-live-p window)
-             (delete-window window))))
        (if-let (win (get-buffer-window buffer-name))
            (delete-window win)
          (let ((buffer (or (cl-loop for buf in (doom-buffers-in-mode 'vterm-mode)
@@ -42,37 +30,22 @@ Returns the vterm buffer."
              (setq-local doom-vterm--id buffer-name)
              (unless (eq major-mode 'vterm-mode)
                (vterm-mode)))
-           ;; HACK forces vterm to redraw, fixing strange artefacting in the tty.
-           (save-window-excursion
-             (pop-to-buffer "*scratch*"))
            (pop-to-buffer buffer)))
        (get-buffer buffer-name)))))
 
-(defun doom-vterm-toggle-project (arg)
+(defun doom-vterm-toggle-project ()
   "Toggles a terminal popup window at current directory.
-
 If prefix ARG is non-nil, recreate vterm buffer in the current directory.
-
 Returns the vterm buffer."
-  (interactive "P")
+  (interactive)
   (setq-local project-root (or (doom-project-root) default-directory))
   (doom-vterm--configure-project-root-and-display
    nil
    (lambda ()
      (let ((buffer-name
-            (format "*doom:vterm-popup:%s*"
-                    (if (bound-and-true-p persp-mode)
-                        (safe-persp-name (get-current-persp))
-                      project-root)))
+            (format "*doom:vterm-popup:%s*" (file-truename project-root)))
            confirm-kill-processes
            current-prefix-arg)
-       (when arg
-         (let ((buffer (get-buffer buffer-name))
-               (window (get-buffer-window buffer-name)))
-           (when (buffer-live-p buffer)
-             (kill-buffer buffer))
-           (when (window-live-p window)
-             (delete-window window))))
        (if-let (win (get-buffer-window buffer-name))
            (delete-window win)
          (let ((buffer (or (cl-loop for buf in (doom-buffers-in-mode 'vterm-mode)
@@ -84,17 +57,12 @@ Returns the vterm buffer."
              (setq-local doom-vterm--id buffer-name)
              (unless (eq major-mode 'vterm-mode)
                (vterm-mode)))
-           ;; HACK forces vterm to redraw, fixing strange artefacting in the tty.
-           (save-window-excursion
-             (pop-to-buffer "*scratch*"))
            (pop-to-buffer buffer)))
        (get-buffer buffer-name)))))
 
 (defun doom-vterm--configure-project-root-and-display (arg display-fn)
   "Sets the environment variable PROOT and displays a terminal using `display-fn`.
-
 If prefix ARG is non-nil, cd into `default-directory' instead of project root.
-
 Returns the vterm buffer."
   (unless (fboundp 'module-load)
     (user-error "Your build of Emacs lacks dynamic modules support and cannot load vterm"))
@@ -105,7 +73,6 @@ Returns the vterm buffer."
             project-root)))
     (setenv "PROOT" project-root)
     (funcall display-fn)))
-
 
 (provide 'doom-vterm-toggle)
 
